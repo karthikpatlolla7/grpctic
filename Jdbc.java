775226ea30f9c5b2cad6e1244ded9ff632bd7abc
Karthik Reddy Patlolla <karthik.p@comakeit.com>
2022-11-29 12:32:24 +0530

    first commit

---
+++ Jdbc.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+public class Jdbc {
+    public static void main(String args[]) throws Exception
+    {
+        try {
+
+
+            String url = "jdbc:mysql://localhost:3306/example";
+            String user = "root";
+            String pass = "root1101";
+            String query = "select * from student";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, user, pass);
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                String ans = rs.getInt(1) +" "+ rs.getString(2);
+                System.out.println(ans);
+            }
+            con.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+
+    }
+
+}
